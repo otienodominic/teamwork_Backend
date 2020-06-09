d4e182ef169eeb519b6256dc4014b6900f806726
otienodominic <otienodominic@gmail.com>
2020-06-09 14:26:27 +0300

    from another comp

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,20 @@
 import express from 'express';
 import controllers from '../controllers';
-import authorize from '../middleware/authorize';
 
 const router = express.Router();
 const { createUser, signIn } = controllers.users;
-const { authRole, authUser } = authorize;
 
-router.post('/create-user', authUser, createUser);
+router.post('/create-user', createUser);
 router.get('/login', signIn);
 // router.get('/users', getAllUsers )
 
-router.get('/dashboard', authUser, (req, res) => {
+router.get('/dashboard', (req, res) => {
   res.json({ message: 'Welcome to the Home Page' });
 });
-router.get('/employee', authUser, (req, res) => {
+router.get('/employee', (req, res) => {
   res.json({ message: 'Welcome to Employee Dashboard' });
 });
-router.get('/admin', authUser, (req, res) => {
+router.get('/admin', (req, res) => {
   res.json({ message: 'Welcome to the Admin Page' });
 });
 
