4ef935aaae8199c87dec5a50d9b125d7dfb81eba
Dominic Ngalo <otienodominic@gmail.com>
2020-06-24 12:50:14 +0300

    Delete index.js

--- src/db/index.js
+++
@@ -1,7 +0,0 @@
-import { Pool } from 'pg';
-
-const connectionString =
-  'postgres://hrvjyfvc:Mb6j-y0zWFPEhSxHUsWuIVfBaa4S13s0@rogue.db.elephantsql.com:5432/hrvjyfvc';
-const pool = new Pool({ connectionString });
-
-export default pool;
