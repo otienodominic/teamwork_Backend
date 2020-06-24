d08ebb13cc8354e010fc550c19bd57d4e3c0934a
Dominic <otienodominic@gmail.com>
2020-06-24 16:00:37 +0300

    added gifs endpoints

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,12 @@
 import users from './users';
 import articles from './article';
 import comments from './comment';
+import gifs from './gif';
 
 // const { createUser, signIn } = users;
 export default {
   users,
   articles,
   comments,
+  gifs,
 };
