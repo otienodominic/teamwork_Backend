56b6d7d401193033ade5785d78d7e84864514b49
Dominic Ngalo <otienodominic@gmail.com>
2020-06-25 07:12:32 +0300

    Revert "added the comments endpoint"

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,8 @@
 import users from './users';
 import articles from './article';
-import comments from './comment';
 
 // const { createUser, signIn } = users;
 export default {
   users,
   articles,
-  comments,
 };
